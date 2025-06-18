6aa9260b84e9d18633d212ebe4b7acdc8f691366
CoelhoVM <vitorya.m@aluno.ifsc.edu.br>
2025-06-18 14:04:46 -0300

    Add '1 Introdução' link to sidebar and create introduction markdown file

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
         // text: 'Guide',
         items: [
           { text: 'Example', link: '/example' },
+          { text: '1 Introdução', link: '/1_introducao' },
           // ...
         ],
       },
